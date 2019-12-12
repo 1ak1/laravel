f96e7a872db9293fbbefaf0eab73fd21691dc252
1ak1 <48246655+1ak1@users.noreply.github.com>
2019-12-12 18:44:16 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - dev
+                    Laravel - dev121313131
                 </div>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
